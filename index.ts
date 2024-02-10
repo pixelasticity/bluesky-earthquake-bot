a58c286371833b8cb38d054a297c8d36fa378ffd
Alexander Bell <contact@alexanderbell.info>
2024-02-10 02:03:57 -0800

    Create API call to get earthquake data

--- index.ts
+++ index.ts
@@ -1,9 +1,72 @@
 import { BskyAgent } from '@atproto/api';
 import * as dotenv from 'dotenv';
-import { CronJob } from 'cron';
-import * as process from 'process';
+// import { CronJob } from 'cron';
+// import * as process from 'process';
 
 dotenv.config();
+
+interface Earthquake {
+	properties: {
+		id: string;
+		type: string;
+		mag: number;
+		time: number;
+		place: string;
+		url: string;
+		title: string;
+	}
+}
+
+function TakeMinutesFromDate(date: Date, minutes: any) {
+    return new Date(date.getTime() - minutes * 60000);
+}
+let now = new Date();
+let fiveMinutesAgo = TakeMinutesFromDate(now, 600);
+let startTime = fiveMinutesAgo.toISOString();
+
+/*
+ * Format: geoJSON
+ * Minimum Magnitude: 1.0
+ * Latitude: 34.14818
+ * Longitude: -118.27332
+ * Maximum Radius: 100 km
+ */
+const apiUrl = "https://earthquake.usgs.gov/fdsnws/event/1/query?format=geojson&minmagnitude=1&latitude=34.14818&longitude=-118.27332&maxradiuskm=100&starttime=" + startTime;
+console.log(apiUrl);
+
+// Make a GET request
+fetch(apiUrl).then(response => {
+	if (!response.ok) {
+		if (response.status === 404) {
+			throw new Error('Data not found');
+		} else if (response.status === 500) {
+			throw new Error('Server error');
+		} else {
+			throw new Error('Network response was not ok');
+		}
+	}
+	return response.json();
+})
+.then(data => {
+	console.log(data.features);
+	data.features.forEach((earthquake: Earthquake) => {
+		let bleatText = "";
+		const magnitude = earthquake.properties.mag,
+					time = new Date(earthquake.properties.time),
+					type = earthquake.properties.type,
+					location = earthquake.properties.place,
+					link = earthquake.properties.url,
+					title = earthquake.properties.title;
+		if (time >= TakeMinutesFromDate(now, 90)) {
+				bleatText = `Earthquake Update: A magnitude ${magnitude} ${type} took place ${location} at ${time.toLocaleTimeString('en-US')}.
+For details from the USGS and to report shaking: ${link}`;
+			post(bleatText, link, title);
+		}
+	})
+})
+.catch(error => {
+	console.error('Error:', error);
+});
 
 // Create a Bluesky Agent
 const agent = new BskyAgent({
@@ -20,10 +83,10 @@ async function main() {
 
 main()
 
-// Run this on a cron job
-const scheduleExpressionMinute = '* * * * *'; // Run once every minute for testing
-const scheduleExpression = '0 */3 * * *'; // Run once every three hours in production
+// // Run this on a cron job
+// const scheduleExpressionMinute = '* * * * *'; // Run once every minute for testing
+// const scheduleExpression = '0 */3 * * *'; // Run once every three hours in production
 
-const job = new CronJob(scheduleExpression, main); // change to scheduleExpressionMinute for testing
+// const job = new CronJob(scheduleExpression, main); // change to scheduleExpressionMinute for testing
 
-job.start();
+// job.start();
