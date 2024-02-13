8414b7da1d79174dfdf7509726e34ab44cbf0840
Alexander Bell <contact@alexanderbell.info>
2024-02-12 19:10:02 -0800

    Remove deploy.ts

--- netlify/functions/deploy.ts
+++
@@ -1,8 +0,0 @@
-import { schedule } from "@netlify/functions";
-export const handler = schedule("* * * * *", async (event: any) => {
-	console.log("Scheduled function executed", Date.now())
-    return {
-        statusCode: 200,
-        body: "Posted"
-    }
-});
