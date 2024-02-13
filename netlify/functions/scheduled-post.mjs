7c2fe53ab4a0f44f117d0b4f5a79a9c5e3418dd5
Alexander Bell <contact@alexanderbell.info>
2024-02-12 20:33:54 -0800

    Create scheduled-post.mjs

---
+++ netlify/functions/scheduled-post.mjs
@@ -0,0 +1,86 @@
+import * as dotenv from 'dotenv';
+let lastPost = "";
+function TakeMinutesFromDate(date, minutes) {
+    return new Date(date.getTime() - minutes * 60000);
+}
+// Create a Bluesky Agent
+// const agent = new BskyAgent({
+//     service: 'https://bsky.social',
+// })
+// async function post(bleat: string, id: string, link: string, title: string, description: string) {
+//     await agent.login({ identifier: process.env.BLUESKY_USERNAME!, password: process.env.BLUESKY_PASSWORD! })
+//     await agent.post({
+//         text: bleat,
+//         langs: [ "en-US" ],
+//         facets: [
+//             {
+//                 index: {
+//                     byteStart: 0,
+//                     byteEnd: 10
+//                 },
+//                 features: [{
+//                     $type: 'app.bsky.richtext.facet#tag',
+//                     tag: '#earthquake'
+//                 }]
+//             }
+//         ],
+//         embed: {
+//             "$type": "app.bsky.embed.external",
+//             "external": {
+//                 "uri": link,
+//                 "title": title + " | USGS",
+//                 "description": description,
+//             }
+//         }
+//     })
+//     console.log("Just posted!")
+//     lastPost = id
+// }
+export default async () => {
+    dotenv.config();
+    console.log('Starting up...');
+    let now = new Date();
+    let fiveMinutesAgo = TakeMinutesFromDate(now, 120);
+    let startTime = fiveMinutesAgo.toISOString();
+    /*
+     * Format: geoJSON
+     * Minimum Magnitude: 1.0
+     * Latitude: 34.14818
+     * Longitude: -118.27332
+     * Maximum Radius: 100 km
+     */
+    const apiUrl = "https://earthquake.usgs.gov/fdsnws/event/1/query?format=geojson&minmagnitude=1&latitude=34.14818&longitude=-118.27332&maxradiuskm=100&starttime=" + startTime;
+    const earthquakes = await fetch(apiUrl)
+        .then(response => {
+        console.log('Fetching data @ %s \nLast post: %s', Date.now(), lastPost);
+        if (!response.ok) {
+            if (response.status === 404) {
+                throw new Error('Data not found');
+            }
+            else if (response.status === 500) {
+                throw new Error('Server error');
+            }
+            else {
+                throw new Error('Network response was not ok');
+            }
+        }
+        return response.json();
+    })
+        .then(data => {
+        data.features.forEach((earthquake) => {
+            console.log(earthquake.id);
+            let bleatText = "";
+            let description = "";
+            const id = earthquake.id, magnitude = earthquake.properties.mag, time = new Date(earthquake.properties.time), type = earthquake.properties.type, location = earthquake.properties.place, link = earthquake.properties.url, title = earthquake.properties.title, latitude = earthquake.geometry.coordinates[0], longitude = earthquake.geometry.coordinates[1], depth = earthquake.geometry.coordinates[2], subBleat = (magnitude >= 2.5 ? ' and to report shaking' : '');
+            if (time >= TakeMinutesFromDate(now, 1)) {
+                bleatText = `Earthquake Update: A magnitude ${magnitude} ${type} took place ${location} at ${time.toLocaleTimeString('en-US')}.
+    For details from the USGS${subBleat}:`;
+                description = `${time.toUTCString()} | ${latitude.toFixed(3)}°N ${longitude.toFixed(3)}°W | ${depth.toFixed(1)} km depth`;
+                console.log(bleatText, id, link, title, description);
+            }
+        });
+    })
+        .catch(error => {
+        console.error('Error:', error);
+    });
+};
